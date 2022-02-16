955fd29906cdd7e5ee83d414ae30a872d8cf957d
Sudip Shrestha <sudipstha08@gmail.com>
2022-02-16 21:40:45 +0545

    feat: cover section

--- src/components/organisms/CoverSection/index.tsx
+++ src/components/organisms/CoverSection/index.tsx
@@ -1,14 +1,42 @@
 import React from 'react'
 import styled from "styled-components"
 
-const Container = styled.div``
+const Container = styled.div`
+  max-width: 1200px;
+  margin: 0 auto;
+  padding-right: 20px;
+  min-height: 800px;
+  & > div {
+    position: relative;
+  }
+  & video {
+    right: 0;
+    top: 0;
+    border-radius: 18px;
+    position: absolute;
+  }
+  & .video-overlay {
+    right: 0;
+    top: 0;
+    bottom: 0;
+    width: 920px;
+    height: 813px;
+    position: absolute;
+    background-color: #b92587;
+    opacity: 0.6;
+    border-radius: 18px;
+  }
+`
 
 const CoverSection = () => {
   return (
     <Container>
-      <video width="920" autoPlay muted playsInline loop>
+      <div>
+<video width="920" autoPlay muted playsInline loop>
         <source src="https://momentonft.com/img/cover.mp4" />
       </video>
+      <div className="video-overlay" />
+      </div>
     </Container>
   )
 }
