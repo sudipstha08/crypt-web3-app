3a8e1039aeb1acc445bc4efa09414c2d7012b5b1
Sudip Shrestha <sudipstha08@gmail.com>
2022-02-18 08:38:58 +0545

    feat: content section

--- src/components/molecules/ContentCard/index.tsx
+++ src/components/molecules/ContentCard/index.tsx
@@ -4,25 +4,28 @@ import styled from 'styled-components'
 const Container = styled.div`
   box-shadow: rgb(118 118 145 / 20%) 0 7px 29px 0;
   padding: 100px 50px;
+  margin: -20px 70px 100px 70px;
+  border-radius: 8px;
   & .info {
     font-size: 27px;
     line-height: 40px;
   }
   & .title {
     font-weight: 48px;
-    font-family: "ITC Avant Garde Gothic Pro";
+    font-family: 'Oswald',sans-serif;
+    font-size: 55px;
   }
   & .desp {
     margin-top: 20px;
     color: #555760;
-    font-size: 1.3rem;
-    line-height: 2.3rem;
+    font-size: 1.15rem;
+    line-height: 2rem;
   }
 `
 
 const ContentCard = ({ info, title, desp }) => {
   return (
-    <Container>
+    <Container className="each-card">
       <div className="info">{info}</div>
       <div className="title">{title}</div>
       <div className="desp">{desp}</div>
--- src/components/organisms/ContentSection/index.tsx
+++ src/components/organisms/ContentSection/index.tsx
@@ -1,8 +1,53 @@
 import React from 'react'
+import styled from 'styled-components'
+import { ContentCard } from "../../molecules"
+
+const Container = styled.div`
+  max-width: 1200px;
+  margin: 0 auto;
+  display: grid;
+  grid-template-columns: 63% auto;
+  grid-gap: 20px;
+  padding-top: 150px;
+  padding-bottom: 100px;
+  position: relative;
+  & .image-wrapper {
+    position: sticky;
+    top: 20px;
+    bottom: 20px;
+    border: 10px solid #000;
+    border-radius: 26px;
+    height: 525px;
+    width: 300px;
+    margin: auto;
+    overflow: hidden;
+  }
+  & .each-card:first-child {
+    margin-bottom: 200px;
+  }
+`
 
 const ContentSection = () => {
   return (
-    <div>ContentSection</div>
+    <Container>
+      <div className="card-wrapper">
+        <ContentCard 
+          info={"Content as an"}
+          title="Asset Class"
+          desp={"We allow content creators to frictionlessly turn their content into NFTs on the blockchain."}
+        />
+         <ContentCard 
+          info={"Candid moments that have"}
+          title="Immediate value"
+          desp={"Track your favorite celebrities, influencers and tastemakers. Don't miss out on a new viral trend ever again."}
+        />
+      </div>
+      <div className='image-wrapper'>
+        <div className="image">
+          <img src="/imgs/content.png" alt="" width="300" height="auto" />
+        </div>
+      </div>
+    </Container>
   )
 }
 
